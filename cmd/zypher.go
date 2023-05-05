e10ad3e0fbcfd7662f6f5a2be13722b312105d86
vtno <tino@vtno.me>
2023-05-05 12:30:06 +0000

    refactor: remove unused code

--- cmd/zypher.go
+++ cmd/zypher.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"os"
 
 	"github.com/mitchellh/cli"
@@ -20,15 +19,6 @@ var (
 	outFile   string
 	inputFile string
 )
-
-func init() {
-	flag.StringVar(&key, "key", "", "key to encrypt/decrypt")
-	flag.StringVar(&key, "k", "", "key to encrypt/decrypt (shorthand)")
-	flag.StringVar(&outFile, "out", "", "output file to be created")
-	flag.StringVar(&outFile, "o", "", "output file to be created (shorthand)")
-	flag.StringVar(&inputFile, "file", "", "input file to be encrypted")
-	flag.StringVar(&inputFile, "f", "", "input file to be encrypted (shorthand)")
-}
 
 func main() {
 	c := cli.NewCLI("zypher", "0.0.1")
